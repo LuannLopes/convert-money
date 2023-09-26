053dc90a00a531f0264b19a84146e87ce75bd173
LuannLopes <luanlopesofc@gmail.com>
2023-09-26 18:13:28 -0300

    Added class to select element and value to option in index.html and updated in script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const convertButton = document.querySelector(".convert-button");
+const currencySelect = document.querySelector(".currency-select");
 
 function convertValues() {
   const inputCurrencyValue = document.querySelector(".input-currency").value;
@@ -8,17 +9,24 @@ function convertValues() {
   const currencyValueConverted = document.querySelector(".currency-value");
 
   const dolarToday = 4.99;
+  const euroToday = 5.28;
 
-  const convertedValue = inputCurrencyValue / dolarToday;
-
-  currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
-    style: "currency",
-    currency: "BRL",
-  }).format(inputCurrencyValue);
-  currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  }).format(convertedValue);
+  if (currencySelect.value == "dolar") {
+    const convertedValueDolar = inputCurrencyValue / dolarToday;
+    currencyValueToConvert.innerHTML = inputCurrencyValue;
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(convertedValueDolar);
+  }
+  if (currencySelect.value == "euro") {
+    const convertedValueEuro = inputCurrencyValue / euroToday;
+    currencyValueToConvert.innerHTML = inputCurrencyValue;
+    currencyValueConverted.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(convertedValueEuro);
+  }
 }
 
 convertButton.addEventListener("click", convertValues);
