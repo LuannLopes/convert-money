1c595ee76b0bc4cc50a18eda215cb78cb6ee86a5
LuannLopes <luanlopesofc@gmail.com>
2023-09-28 16:46:10 -0300

    Update index.html and script.js

--- script.js
+++ script.js
@@ -32,13 +32,18 @@ function convertValues() {
 
 function changeCurrency() {
   const currencyName = document.getElementById("currency-name");
+  const currencyImage = document.querySelector(".currency-img");
 
   if (currencySelect.value == "dolar") {
     currencyName.innerHTML = "Dólar americano";
+    currencyImage.src = "img/bandeira-eua.png";
   }
   if (currencySelect.value == "euro") {
     currencyName.innerHTML = "Euro";
+    currencyImage.src = "img/bandeira-europa.png";
   }
+
+  convertValues();
 }
 
 currencySelect.addEventListener("change", changeCurrency);
